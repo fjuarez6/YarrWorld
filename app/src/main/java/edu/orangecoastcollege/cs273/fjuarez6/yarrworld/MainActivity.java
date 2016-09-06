68652ec8cc71b1d827953d54d31b139ba6267f3b
freddy <frddyjrz@gmail.com>
2016-09-06 16:24:51 -0700

    final

--- app/src/main/java/edu/orangecoastcollege/cs273/fjuarez6/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/fjuarez6/yarrworld/MainActivity.java
@@ -1,13 +1,35 @@
 package edu.orangecoastcollege.cs273.fjuarez6.yarrworld;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button speakButton;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hook up the speakButton to the one in our view:
+        speakButton = (Button) findViewById(R.id.speakButton);
+
+        //Wire up speakButton to perform an onClick event
+        speakButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Use the static reference to "Toast" to display the message:
+                        // Toast = vanishes after set amount of time
+                        // Dialog = requires user input
+                        Toast.makeText(context, R.string.toast_text, Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
